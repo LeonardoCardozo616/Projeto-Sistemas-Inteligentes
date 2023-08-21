a5e4d7d6eea141713c0d562d39517603a5d60431
Leonardo Cardozo <leon52.cpere@outlook.com>
2023-08-20 21:36:43 -0300

    Aprimorando mochila.py

--- mochila.py
+++ mochila.py
@@ -2,16 +2,25 @@ import random
 
 class KnapsackPackage(object):
      """ Criação da Mochila """
-     def __init__(self, weight, value, n):
-         self.weight = [weight[i] for i in range(n)]
-         self.value = [value[i] for i in range(n)]
-         self.n = n 
-         self.cost = [value[i] / weight[i] for i in range(n)] 
+     def __init__(self, max):
+         self.max = max
+         self.weight = 0
+         self.value = 0
+
+     def adicionarItem(self, weight, value):
+          if((self.weight + weight) <= self.max):
+               self.weight += weight
+               self.value += value
+
+     def removerItem(self, weight, value):
+          self.weight -= weight
+          self.value -= value
  
 if __name__ == "__main__":
      W = [15, 10, 2, 4] # Peso dos Itens
      V = [30, 25, 2, 6] # Valor dos Itens
      M = 37 # Carga máxima da mochila
      n = 4 # Número de Itens
-     mochila = KnapsackPackage(W, V, n)
-     print(mochila.cost)
+     
+     mochila = KnapsackPackage(M)
+     print(mochila.value)
