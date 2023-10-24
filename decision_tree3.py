c576d8c36eba7a78d16f5108b5e54aa1963d454e
Leonardo Cardozo <leon52.cpere@outlook.com>
2023-10-24 09:40:19 -0300

    Aprimorando meu código.

--- decision_tree3.py
+++ decision_tree3.py
@@ -5,76 +5,71 @@ from sklearn.metrics import accuracy_score
 
 class Node():
     def __init__(self, feature_index=None, threshold=None, left=None, right=None, info_gain=None, value=None):
-        ''' constructor ''' 
-        
-        # for decision node
+        # Nó de decisão
         self.feature_index = feature_index
         self.threshold = threshold
         self.left = left
         self.right = right
         self.info_gain = info_gain
         
-        # for leaf node
+        # Nó folha
         self.value = value
 
 
 class DecisionTreeClassifier():
     def __init__(self, min_samples_split=2, max_depth=2):
-        ''' constructor '''
-        
-        # initialize the root of the tree 
+        # raiz da árvore 
         self.root = None
         
-        # stopping conditions
+        # limites
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         
+    ''' Função recursiva que cria a árvore ''' 
     def build_tree(self, dataset, curr_depth=0):
-        ''' recursive function to build the tree ''' 
-        
         X, Y = dataset[:,:-1], dataset[:,-1]
         num_samples, num_features = np.shape(X)
         
-        # split until stopping conditions are met
+        # Separa até que as condições de parada sejam encontradas
         if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
-            # find the best split
+            # encontra a melhor divisão
             best_split = self.get_best_split(dataset, num_samples, num_features)
-            # check if information gain is positive
+            # verifica se o ganho de informação é positivo
             if best_split["info_gain"]>0:
-                # recur left
+                # cria a árvore esquerda
                 left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
-                # recur right
+                # cria a árvore direita
                 right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
-                # return decision node
+                # retorna o nó de decisão
                 return Node(best_split["feature_index"], best_split["threshold"], 
                             left_subtree, right_subtree, best_split["info_gain"])
         
-        # compute leaf node
+        # calcula o nó folha
         leaf_value = self.calculate_leaf_value(Y)
-        # return leaf node
+        # retorna o nó folha
         return Node(value=leaf_value)
     
     def get_best_split(self, dataset, num_samples, num_features):
-        ''' function to find the best split '''
+        ''' encontra a melhor divisão '''
         
-        # dictionary to store the best split
+        # best_split é um dicionário
         best_split = {}
-        max_info_gain = -float("inf")
+        max_info_gain = -float("inf") #Valor inicial um - infinito
         
-        # loop over all the features
+        # loop para todos os recursos (features)
         for feature_index in range(num_features):
             feature_values = dataset[:, feature_index]
             possible_thresholds = np.unique(feature_values)
-            # loop over all the feature values present in the data
+            # Loop para todos os valores presentes nos dados
             for threshold in possible_thresholds:
-                # get current split
+                # pegar divisão atual
                 dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
-                # check if childs are not null
+                # Verifica se os filhos não são nulos
                 if len(dataset_left)>0 and len(dataset_right)>0:
                     y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
-                    # compute information gain
+                    # Computa o ganho de informação
                     curr_info_gain = self.information_gain(y, left_y, right_y, "gini")
-                    # update the best split if needed
+                    # Sempre encontra a melhor divisão
                     if curr_info_gain>max_info_gain:
                         best_split["feature_index"] = feature_index
                         best_split["threshold"] = threshold
@@ -83,18 +78,18 @@ class DecisionTreeClassifier():
                         best_split["info_gain"] = curr_info_gain
                         max_info_gain = curr_info_gain
                         
-        # return best split
+        # retorna best_split
         return best_split
     
     def split(self, dataset, feature_index, threshold):
-        ''' function to split the data '''
+        ''' função que divide o dataset '''
         
         dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
         dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
         return dataset_left, dataset_right
     
     def information_gain(self, parent, l_child, r_child, mode="entropy"):
-        ''' function to compute information gain '''
+        ''' função que calcula o ganho de informação '''
         
         weight_l = len(l_child) / len(parent)
         weight_r = len(r_child) / len(parent)
@@ -105,7 +100,7 @@ class DecisionTreeClassifier():
         return gain
     
     def entropy(self, y):
-        ''' function to compute entropy '''
+        ''' Entropia '''
         
         class_labels = np.unique(y)
         entropy = 0
@@ -115,7 +110,7 @@ class DecisionTreeClassifier():
         return entropy
     
     def gini_index(self, y):
-        ''' function to compute gini index '''
+        ''' Impuridade Gini '''
         
         class_labels = np.unique(y)
         gini = 0
@@ -125,42 +120,41 @@ class DecisionTreeClassifier():
         return 1 - gini
         
     def calculate_leaf_value(self, Y):
-        ''' function to compute leaf node '''
+        ''' calcula o nó folha '''
         
         Y = list(Y)
         return max(Y, key=Y.count)
     
-    def print_tree(self, tree=None, indent=" "):
-        ''' function to print the tree '''
+    def print_tree(self, tree=None, indent=" ", col_name=None, results=None):
+        ''' impressão da árvore '''
         
         if not tree:
             tree = self.root
 
         if tree.value is not None:
-            print(tree.value)
+            print(' ',results[int(tree.value) - 1])
 
         else:
-            print("X_"+str(tree.feature_index), "<=", tree.threshold, "?", tree.info_gain)
+            print(col_name[tree.feature_index], "<=", tree.threshold, "?", " info gain: {:.2f}".format(tree.info_gain))           
             print("%sleft:" % (indent), end="")
-            self.print_tree(tree.left, indent + indent)
+            self.print_tree(tree.left, indent + indent, col_name=col_name, results=results)
             print("%sright:" % (indent), end="")
-            self.print_tree(tree.right, indent + indent)
+            self.print_tree(tree.right, indent + indent, col_name=col_name, results=results)
     
     def fit(self, X, Y):
-        ''' function to train the tree '''
+        ''' inicia a árvore '''
         
         dataset = np.concatenate((X, Y), axis=1)
         self.root = self.build_tree(dataset)
-        self.print_tree()
     
     def predict(self, X):
-        ''' function to predict new dataset '''
+        ''' realiza uma previsão '''
         
         preditions = [self.make_prediction(x, self.root) for x in X]
         return preditions
     
     def make_prediction(self, x, tree):
-        ''' function to predict a single data point '''
+        ''' prevê um dado isolado '''
         
         if tree.value!=None: return tree.value
         feature_val = x[tree.feature_index]
@@ -171,6 +165,7 @@ class DecisionTreeClassifier():
 
 
 col_names = ['i', 'pSist', 'pDiast', 'qPA', 'pulso', 'resp', 'gravid', 'classe']
+resultados = ['crítico', 'instável', 'potencialmente estável', 'estável']
 data = pd.read_csv("treino_sinais_vitais_com_label.csv", header=None, names=col_names)
 print(data.head(10))
 
@@ -181,6 +176,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.2, random_s
 
 classifier = DecisionTreeClassifier(min_samples_split=4, max_depth=4)
 classifier.fit(X_train,Y_train)
+classifier.print_tree(col_name=col_names, results=resultados)
 
 Y_pred = classifier.predict(X_test) 
 print('Nível de precisão: ', accuracy_score(Y_test, Y_pred))
